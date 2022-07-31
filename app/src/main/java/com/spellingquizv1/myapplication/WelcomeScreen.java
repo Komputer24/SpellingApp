5233739350393ef0c4e8e33ca2bf5a8b0ab77c8c
Kaleab <kaleabnb24@gmail.com>
2022-07-31 00:14:13 -0400

    added popup 2

--- app/src/main/java/com/spellingquizv1/myapplication/WelcomeScreen.java
+++ app/src/main/java/com/spellingquizv1/myapplication/WelcomeScreen.java
@@ -3,17 +3,32 @@ package com.spellingquizv1.myapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeScreen extends AppCompatActivity {
     private Button startBtn;
+    public void settingClick(View v) {
+        //openSettings();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Coming Soon");
+        builder.setMessage("This is currently in development, try out the other features.");
+
+        // add a button
+        builder.setPositiveButton("OK", null);
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Hides Title Bar
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -22,6 +37,7 @@ public class WelcomeScreen extends AppCompatActivity {
         //getSupportActionBar().hide();
 
         setContentView(R.layout.welcome_screen);
+        setupHyperlink();
         // Program Start
         // Version Control Lets See
         startBtn = (Button) findViewById(R.id.startBtn);
@@ -44,7 +60,9 @@ public class WelcomeScreen extends AppCompatActivity {
     public void imageButtonOnClick(View v) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.aaed.org"));
         startActivity(browserIntent);
-    } public void settingClick(View v) {
-        openSettings();
+    }
+    private void setupHyperlink() {
+        TextView linkTextView = findViewById(R.id.privacy);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
